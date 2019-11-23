23be9e33af4b1848b595f683a1ebaa497f4608cd
Nikolay Sergeychuk <nicknixer@gmail.com>
2019-11-23 20:23:58 +0300

    init project on spring boot

---
+++ src/main/java/blog/App.java
@@ -0,0 +1,11 @@
+package blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
